c29635281ea732fe67b29d7547dab184d2372238
jrobinsonvm <jamesro@vmware.com>
2022-08-25 14:46:34 -0400

    added auto expand and iam capability

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-aws cloudformation deploy --template-file=amazon-eks-entrypoint-existing-vpc.template.yaml  --stack-name=eks01
+aws cloudformation deploy --template-file=amazon-eks-entrypoint-existing-vpc.template.yaml  --stack-name=eks-devtest-01  --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
